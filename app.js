a1e53d641a5967487da81380d8cab94ca76f1ed3
jorgeceroideas <jorge@ceroideas.es>
2024-10-10 21:51:56 +0200

    cambios http

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
 const cors = require('cors');
@@ -38,4 +38,38 @@ const options = {
 
 https.createServer(options, app).listen(port, () => {
   console.log(`Servidor escuchando en https://localhost:${port}`);
+});*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const driverRouter = require('./routes/drivers');
+const zonesRouter = require('./routes/zones');
+const productsRouter = require('./routes/products');
+const ganttRouter = require('./routes/gantt');
+const servicesRouter = require('./routes/services');
+const servicesmapRouter = require('./routes/servicesmap');
+const availabilitiesRouter = require('./routes/availabilities');
+const clientsRouter = require('./routes/clients');
+const algorithmsRouter = require('./routes/algorithms');
+
+app.use('/drivers', driverRouter);
+app.use('/zones', zonesRouter);
+app.use('/products', productsRouter);
+app.use('/gantt', ganttRouter);
+app.use('/services', servicesRouter);
+app.use('/services-map', servicesmapRouter);
+app.use('/availabilities', availabilitiesRouter);
+app.use('/clients', clientsRouter);
+app.use('/algorithms', algorithmsRouter);
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
